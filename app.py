31f45539fb5b21cdb8818474614a84f30d999877
minju <88606886+meanjoo@users.noreply.github.com>
2023-05-16 11:17:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 x = st.slider('Select a value')
 st.write(x, 'squared is', x*x)
 
-st.select_slider('Pick a mark', ['Bad, 'Good', 'Excellent'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
